4fb9bd0be7312f79ddb4564067af4ff230333e0a
Daniel Riddersporre <daniel@riddersporre.se>
2025-11-20 12:01:24 +0100

    project base setup and tailwind added

---
+++ Blotto_UI/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='bg-violet-400 p-2 rounded-md'>Blotto</p>
+      </div>
+    </>
+  )
+}
+
+export default App

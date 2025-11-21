703116ce412307610ea9a25f1826cbb1c4af1dce
Daniel Riddersporre <daniel@riddersporre.se>
2025-11-21 14:17:28 +0100

    Button menu is done for now. Added comments that discribes the current problem and future demands.

--- Blotto_UI/src/App.tsx
+++ Blotto_UI/src/App.tsx
@@ -5,15 +5,21 @@ function App() {
   return (
     <>
       <div className='flex flex-col bg-violet-200 h-full'>
-        <div className='flex justify-around py-3 items-start bg-violet-950'>
-          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>Skapa Bingo</button>
-          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>1</button>
-          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>2</button>
-          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>3</button>
-          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>4</button>
-          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>5</button>
+        <div className='flex justify-between p-3 items-start bg-violet-950'>
+          <div>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2'>Skapa Bingo</button>
+          </div>
+          <div className="flex overflow-scroll">
+            {/*Make this part horizontally scrollable and buttons should be added dynamically */}
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>1</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>2</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>3</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>4</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>5</button>
+          </div>
         </div>
         <div className='flex-1 p-2'>
+          {/*This part is for the bingo card and it should be added dynamically*/}
           <p className=''>TESTAR</p>
           <p className=''>TESTAR</p>
           <p className=''>TESTAR</p>

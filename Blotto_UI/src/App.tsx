d5d03b0962b741b517cfbe7227e43328f01a9237
Daniel Riddersporre <daniel@riddersporre.se>
2025-11-21 13:16:08 +0100

    Menu bar more or less done

--- Blotto_UI/src/App.tsx
+++ Blotto_UI/src/App.tsx
@@ -4,8 +4,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <p className='bg-violet-400 p-2 rounded-md'>Blotto</p>
+      <div className='flex flex-col bg-violet-200 h-full'>
+        <div className='flex justify-around py-3 items-start bg-violet-950'>
+          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>Skapa Bingo</button>
+          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>1</button>
+          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>2</button>
+          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>3</button>
+          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>4</button>
+          <button className='text-white font-bold rounded-md bg-violet-500 p-2'>5</button>
+        </div>
+        <div className='flex-1 p-2'>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+          <p className=''>TESTAR</p>
+        </div>
       </div>
     </>
   )

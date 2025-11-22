9ff7efed056264e06d7b1f4b62a69c72c1af06a5
Daniel Riddersporre <daniel@riddersporre.se>
2025-11-22 16:12:38 +0100

    Scroll in navigation menu added and is working as intended

--- Blotto_UI/src/App.tsx
+++ Blotto_UI/src/App.tsx
@@ -4,18 +4,20 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col bg-violet-200 h-full'>
-        <div className='flex justify-between p-3 items-start bg-violet-950'>
-          <div>
+      <div className='flex flex-col bg-violet-200 h-full w-full overflow-hidden'>
+        <div className='flex p-3 items-start bg-violet-950'>
+          <div className='min-w-[35%]'>
             <button className='text-white font-bold rounded-md bg-violet-500 p-2'>Skapa Bingo</button>
           </div>
-          <div className="flex overflow-scroll">
+          <div className="flex overflow-x-auto space-x-3 no-scrollbar">
             {/*Make this part horizontally scrollable and buttons should be added dynamically */}
-            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>1</button>
-            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>2</button>
-            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>3</button>
-            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>4</button>
-            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10'>5</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>1</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>2</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>3</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>4</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>5</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>6</button>
+            <button className='text-white font-bold rounded-md bg-violet-500 p-2 w-10 flex-shrink-0'>7</button>
           </div>
         </div>
         <div className='flex-1 p-2'>
